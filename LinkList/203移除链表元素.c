267783ea738c3c3cb443b8ec988d5ebc0f20091e
ink <541640794@qq.com>
2021-03-21 11:44:04 +0800

    update 203

--- LinkList/203移除链表元素.c
+++ LinkList/203移除链表元素.c
@@ -5,7 +5,6 @@
  *     struct ListNode *next;
  * };
  */
-//前导0
 struct ListNode* removeElements(struct ListNode* head, int val) {
   struct ListNode* pre = (struct ListNode*)malloc(sizeof(struct ListNode));
   while (head) {
@@ -16,7 +15,6 @@ struct ListNode* removeElements(struct ListNode* head, int val) {
     }
   }
   pre = head;
-  //   [1 1 1 2 3 2 2 1] 2
   while (head) {
     struct ListNode* tmp = head;
     while (tmp->next && tmp->next->val == val) {
@@ -31,18 +29,20 @@ struct ListNode* removeElements(struct ListNode* head, int val) {
 struct ListNode* removeElements(struct ListNode* head, int val) {
   //   哨兵节点广泛应用于树和链表中，如伪头、标记等，通常不保存任何数据
   //   其主要目的是使链表标准化，如使链表永不为空、永不无头、简化插入和删除。
-  //   伪头
-  struct ListNode* s = (struct ListNode*)malloc(sizeof(struct ListNode));
-  s->next = head;
+  struct ListNode* pre = (struct ListNode*)malloc(sizeof(struct ListNode));
+  pre->next = head;
+  //双指针
+  struct ListNode* p = pre;
   struct ListNode* cur = head;
-  struct ListNode* pre = s;
   while (cur) {
     if (cur->val == val) {
-      pre->next = cur->next;
+      cur = cur->next;
+      p->next = cur;
+
     } else {
-      pre = cur;
+      p = p->next;
+      cur = cur->next;
     }
-    cur = cur->next;
   }
-  return s->next;
+  return pre->next;
 }
