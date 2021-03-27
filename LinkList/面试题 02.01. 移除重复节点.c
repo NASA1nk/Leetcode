52bbc61e5b56f706ebb5cbe3be4dbd003be090f7
ink <541640794@qq.com>
2021-03-27 12:32:50 +0800

    Update 面试题 02.01. 移除重复节点.c

--- LinkList/面试题 02.01. 移除重复节点.c
+++ LinkList/面试题 02.01. 移除重复节点.c
@@ -6,11 +6,22 @@
  * };
  */
 
-// 未排序链表
-// 当前节点只用和后面节点比较
-// 删除节点变种，值变成所有节点的值
+// 未排序链表-hash(链表元素在[0, 20000]范围内)
+// 203删除节点变种，单值变成所有节点的值
 struct ListNode* removeDuplicateNodes(struct ListNode* head) {
   struct ListNode* pre = (struct ListNode*)malloc(sizeof(struct ListNode));
-  struct ListNode* s = (struct ListNode*)malloc(sizeof(struct ListNode));
   pre->next = head;
+  int* hash = (int*)malloc(sizeof(int) * 20001);
   struct ListNode* cur;
+  while (head) {
+    cur = head->next;
+    if (hash[head->val] == 0) {
+      hash[head->val] = 1;
+    } else {
+      pre->next = cur->next;
+    }
+    pre = pre->next;
+    head = head->next;
+  }
+  return pre->next;
+}
