3ed9584e6e7008d6a520b320bfa76cac392cf064
ink <541640794@qq.com>
2021-03-28 16:37:30 +0800

    Update 20. 有效的括号.java

--- 栈/20. 有效的括号.java
+++ 栈/20. 有效的括号.java
@@ -1,20 +1,20 @@
-import java.util.Stack;
-
+// Java不推荐用栈，而是用双端队列
 class Solution {
     public boolean isValid(String s) {
         if (s.length() % 2 == 1) {
             return false;
         }
+        // Character类型
         Stack<Character> k = new Stack<Character>();
         for (int i = 0; i < s.length(); i++) {
             if (!k.empty()) {
                 if ((s.charAt(i) == ')' && k.peek() == '(') || (s.charAt(i) == ']' && k.peek() == '[')
                         || (s.charAt(i) == '}' && k.peek() == '{')) {
                     k.pop();
-                } else {
-                    k.push(s.charAt(i));
+                    continue;
                 }
             }
+            k.push(s.charAt(i));
         }
         if (!k.empty()) {
             return false;
