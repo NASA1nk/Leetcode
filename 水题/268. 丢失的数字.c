abe8c9b294dfbd7eb88f6075cff1e6f775d0b024
ink <541640794@qq.com>
2021-03-28 21:59:20 +0800

    Delete 268. 丢失的数字.c

--- 水题/268. 丢失的数字.c
+++
@@ -1,8 +0,0 @@
-int missingNumber(int* nums, int numsSize) {
-  int sum = 0;
-  int s = numsSize * (numsSize + 1) / 2;
-  for (int i = 0; i < numsSize; i++) {
-    sum += nums[i];
-  }
-  return s - sum;
-}
