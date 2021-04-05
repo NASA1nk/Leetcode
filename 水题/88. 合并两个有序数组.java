7fed36043b350af56a0d0bb08c206ce24ccc01e0
ink <541640794@qq.com>
2021-04-05 14:55:39 +0800

    Create 88. 合并两个有序数组.java

---
+++ 水题/88. 合并两个有序数组.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+      int l = m + n - 1;
+      int m1 = m - 1;
+      int n1 = n - 1;
+      while (m1 >= 0 && n1 >= 0) {
+        if (nums1[m1] > nums2[n1]) {
+          nums1[l] = nums1[m1];
+          m1--;
+        } else {
+          nums1[l] = nums2[n1];
+          n1--;
+        }
+        l--;
+      }
+      while (n1 >= 0) {
+        nums1[l--] = nums2[n1--];
+      }
+    }
+  }
