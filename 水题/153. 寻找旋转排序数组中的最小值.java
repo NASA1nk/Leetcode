125238db9bb8f01eb6f51925785ce2a65200b9b3
ink <541640794@qq.com>
2021-04-08 00:29:12 +0800

    Update 153. 寻找旋转排序数组中的最小值.java

--- 水题/153. 寻找旋转排序数组中的最小值.java
+++ 水题/153. 寻找旋转排序数组中的最小值.java
@@ -1,11 +1,10 @@
 class Solution {
     public int findMin(int[] nums) {
-        int ans = nums[0];
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i + 1] < nums[i]) {
-                return ans < nums[i + 1] ? ans : nums[i + 1];
+                return nums[i + 1];
             }
         }
-        return ans;
+        return nums[0];
     }
 }
