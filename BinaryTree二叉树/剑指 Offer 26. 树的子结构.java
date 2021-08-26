dcc0aa978428497c1d1af3906c809b0b8ec24586
ink <541640794@qq.com>
2021-08-26 22:49:28 +0800

    Create 剑指 Offer 26. 树的子结构.java

---
+++ BinaryTree二叉树/剑指 Offer 26. 树的子结构.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// 递归
+// 必须判断A的每一个节点，是否是B的root，因为可能多个节点都可能是根节点
+class Solution {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if(A == null || B == null){
+            return false;
+        }
+        return traversal(A,B) || isSubStructure(A.left,B) || isSubStructure(A.right,B);
+    }
+    // 判断是否包含
+    public boolean traversal(TreeNode A,TreeNode B){
+        // B为null一定包含
+        if(B == null){
+            return true;
+        }
+        if(A == null || A.val != B.val){
+            return false;
+        }
+        return traversal(A.left,B.left) && traversal(A.right,B.right);
+    }
+}
