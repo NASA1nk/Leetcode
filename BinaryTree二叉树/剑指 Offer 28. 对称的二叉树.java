be5cb0bad00cac41495f89a155951c5908e77ec6
ink <541640794@qq.com>
2021-09-29 17:35:59 +0800

    Create 剑指 Offer 28. 对称的二叉树.java

---
+++ BinaryTree二叉树/剑指 Offer 28. 对称的二叉树.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        return isSymmetriciml(root.left,root.right); 
+    }
+    public boolean isSymmetriciml(TreeNode left,TreeNode right){
+        if(left == null && right == null){
+            return true;
+        }
+        if(left == null || right == null){
+            return false;
+        }
+        if(left.val != right.val){
+            return false;
+        }
+        return isSymmetriciml(left.left,right.right) && isSymmetriciml(left.right,right.left);
+    }
+}
